b51aecda166c8dc0343bcf07a7ef26aaaca5ea6c
devamcodes <devamagrawal.codes@gmail.com>
2020-07-27 21:46:48 +0530

    access key added but results are not as expected.

--- project_dict.py
+++ project_dict.py
@@ -1,4 +1,9 @@
 my_dict = {"T-shirt": {"size": ["Small", "Medium", "Large"], "Colour": ["Red", "Blue", "Green"]},
            "Shirt": {"size": ["Small", "Medium", "Large"], "Colour": ["Red", "Blue", "Green"]},
            "Jacket": {"size": ["Small", "Medium", "Large"], "Colour": ["Red", "Blue", "Green"]}}
+for key in my_dict:
+    chioce = input("ENTER THE TYPE:")
+    print(f"in {key} there are three sizes and three colours ")
+    print(my_dict[key])
+
 print(my_dict)
