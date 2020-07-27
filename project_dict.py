50a06f26546f73b6390ff5ff7f86f6679e98eff8
devamcodes <devamagrawal.codes@gmail.com>
2020-07-27 20:09:59 +0530

    nested dictionry test.

---
+++ project_dict.py
@@ -0,0 +1,4 @@
+my_dict = {"T-shirt": {"size": ["Small", "Medium", "Large"], "Colour": ["Red", "Blue", "Green"]},
+           "Shirt": {"size": ["Small", "Medium", "Large"], "Colour": ["Red", "Blue", "Green"]},
+           "Jacket": {"size": ["Small", "Medium", "Large"], "Colour": ["Red", "Blue", "Green"]}}
+print(my_dict)
